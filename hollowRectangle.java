40721a027fa69656ac7ad02b8bdb94baed62e435
harshs0203 <harshs0203@gmail.com>
2023-04-02 01:17:35 +0530

    runnable

--- hollowRectangle.java
+++ hollowRectangle.java
@@ -16,9 +16,11 @@ public class hollowRectangle {
         //for rows
         for(int i = 0; i < rows; i++){
             //for columns
-            for(int j = 0; j < columns;j++){
-               if( i == 0 || j ==0 || i == rows || j==columns ){
+             for(int j = 0; j < columns; j++){
+               if( i == 0 || j == 0 || i == rows-1 || j==columns-1 ){
                 System.out.print(" * ");
+               }else{
+                System.out.print(" ");
                }
             }
             System.out.println();
