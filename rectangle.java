9cdf7485a227400a37b54784d8a7b6fef91154f5
harshs0203 <harshs0203@gmail.com>
2023-04-02 00:37:30 +0530

    running the code

--- rectangle.java
+++ rectangle.java
@@ -1,3 +1,29 @@
-class Rectangle{
+import java.util.Scanner;
+
+class Rectangle {
+
+public static void main(String[] args) {
     
+ int rows;
+int columns;
+Scanner s = new Scanner(System.in);
+
+System.out.print("Enter the numbr of rows  ");
+rows = s.nextInt();
+
+System.out.print("Enter the numbr of columns  ");
+columns = s.nextInt();
+
+//for rows
+for(int i = 0; i < rows; i++){
+    //for columns
+    for(int j = 0; j < columns;j++){
+        System.out.print(" * ");
+    }
+    System.out.println();
+}
+
+s.close();
+}
+
 }
